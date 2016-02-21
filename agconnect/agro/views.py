0a097bbfb5c15270dc2f9243cede6169cc270f6d
Wyatt Cook <wyattcook2018@u.northwestern.edu>
2016-02-20 23:18:15 -0600

    Goes through 2 different fields from user

--- agconnect/agro/views.py
+++ agconnect/agro/views.py
@@ -78,15 +78,22 @@ def homepage(request):
 			break
 		except:
 			pass
-	longi = d['fields'][1]['centroid']['coordinates'][0]
+	longi = []
+	lat =[]
+	longi.append( d['fields'][0]['centroid']['coordinates'][0])
+	lat.append( d['fields'][0]['centroid']['coordinates'][1])
+	longi.append( d['fields'][1]['centroid']['coordinates'][0])
 	print longi
-	lat = d['fields'][1]['centroid']['coordinates'][1]
+	lat.append( d['fields'][1]['centroid']['coordinates'][1])
 	print lat
 	codes = []
-	codes=geo_helper(lat,longi)
+	codes=geo_helper(lat[1],longi[1])
 	print codes
+	codes2=[]
+	codes2=geo_helper(lat[0],longi[0])
 	#params = {"key": API_KEY, "commodity_desc": "CORN", "year":"2012","begin_code_alpha":"1", "end_code_alpha":"2", "county_code_alpha": "001", "state_alpha": "CT", "format": "JSON"}
 	state_val=""
+	state_val2=""
 	if codes[0]=="17":
 		state_val="IL"
 	elif codes[0]=="09":
@@ -98,14 +105,24 @@ def homepage(request):
 	# 001 CT
 	# 061 IA
 	# 027 NJ
-	print state_val
+	if codes2[0]=="17":
+		state_val2="IL"
+	elif codes2[0]=="09":
+		state_val2="CT"
+	elif codes2[0]=="19":
+		state_val2="IA"
+	elif codes2[0]=="34":
+		state_val2="NJ"
+	print state_val2
 	params = {"key": API_KEY, "commodity_desc": "CORN", 
 	"year":"2012","begin_code_alpha":"1", "end_code_alpha":"12", "county_code_alpha":codes[1], 
 	"state_alpha": state_val, "format": "JSON"}
+	params2 = {"key": API_KEY, "commodity_desc": "CORN", 
+	"year":"2012","begin_code_alpha":"1", "end_code_alpha":"12", "county_code_alpha":codes2[1], 
+	"state_alpha": state_val2, "format": "JSON"}
+	r2=requests.get(geturl, params2)
 	r = requests.get(geturl, params)
 	d = json.loads(r.text)
-	print d
-	myjson =[]
 	tons =0
 	tonscount=0
 	tonsperacre=0
@@ -129,7 +146,6 @@ def homepage(request):
 			elif j['unit_desc']=="TONS / ACRE":
 				tonsperacrecount+=1
 				tonsperacre += intval
-			myjson.append([j['unit_desc'], j['Value']])
 		except:
 			pass
 			#print j['unit_desc']
@@ -137,13 +153,46 @@ def homepage(request):
 	#so far this is arbitrary.  time to make it take an input of a JSON object of all the fields lat and long and names
 	# then pass it into geo_view which i will change from a view
 	#into a function that takes two parameters, lat and longi.  Then display the stuff I was doing before. 
-	newjson={}
+	newjson=[{}, {}]
 	if dollarscount>0:
-		newjson['$']=[dollars,dollarscount,dollars/dollarscount]
+		newjson[0]['$']=[dollars,dollarscount,dollars/dollarscount]
 	if tonscount>0:
-		newjson['TONS']=[tons, tonscount, tons/tonscount]
+		newjson[0]['TONS']=[tons, tonscount, tons/tonscount]
 	if tonsperacrecount>0:
-		newjson["TONS / ACRE"] = [tonsperacre, tonsperacrecount, tonsperacre/tonsperacrecount]
+		newjson[0]["TONS / ACRE"] = [tonsperacre, tonsperacrecount, tonsperacre/tonsperacrecount]
+	d=json.loads(r2.text)
+	tons =0
+	tonscount=0
+	tonsperacre=0
+	tonsperacrecount=0
+	dollars=0
+	dollarscount=0
+	for j in d['data']:
+		val=j['Value']
+		oldval=val
+		val=val.replace(',', '')
+		try:
+			intval=int(val)
+		#try: 
+			if j['unit_desc']=="$":
+				
+				dollars+=intval
+				dollarscount+=1
+			elif j['unit_desc']=="TONS":
+				tons+=intval
+				tonscount+=1
+			elif j['unit_desc']=="TONS / ACRE":
+				tonsperacrecount+=1
+				tonsperacre += intval
+		except:
+			pass
+	if dollarscount>0:
+		newjson[1]['$']=[dollars,dollarscount,dollars/dollarscount]
+	if tonscount>0:
+		newjson[1]['TONS']=[tons, tonscount, tons/tonscount]
+	if tonsperacrecount>0:
+		newjson[1]["TONS / ACRE"] = [tonsperacre, tonsperacrecount, tonsperacre/tonsperacrecount]
+
 #	newjson =  {"$": [dollars, dollarscount, dollars/dollarscount], "TONS": [tons, tonscount, tons/tonscount], "TONS / ACRE": [tonsperacre, tonsperacrecount, tonsperacre/tonsperacrecount]}
 	return render(request, "agro/index.html", {"stuff": newjson})	
 
